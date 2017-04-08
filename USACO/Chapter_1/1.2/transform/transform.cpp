ec94c77e59be96b4b8121ab37f0ce595401658f4
Abhinav Diddee <adiddee@live.com>
2017-04-08 01:50:51 +0000

    transform done

--- USACO/Chapter_1/1.2/transform/transform.cpp
+++ USACO/Chapter_1/1.2/transform/transform.cpp
@@ -1,9 +1,19 @@
+/*
+ID: adiddee1
+PROG: transform
+LANG: C++11
+*/
 #include <iostream>
 #include <fstream>
 #include <cstdio>
 using namespace std;
-char[][] rotate(char matrix[] [], int size);
-bool compare(char m1[][], char m2[][], int size);
+
+bool compare(char mat1[10][10], char mat2[10][10], int size);
+bool rotate90test(char mat1[10][10], char mat2[10][10], int size);
+bool rotate180test(char mat1[10][10], char mat2[10][10], int size);
+bool rotate270test(char mat1[10][10], char mat2[10][10], int size);
+bool mirrortest(char mat1[10][10], char mat2[10][10], int size);
+bool combotest(char mat1[10][10], char mat2[10][10], int size);
 
 int main()
 {
@@ -11,5 +21,161 @@ int main()
     ofstream out("transform.out");
     int size;
     in >> size;
-    char matrix[size] [size];
+    in.get();
+    char mat1[10] [10];
+    char mat2[10] [10];
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat1[i][j] = in.get();
+        }
+        in.get();
+    }
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat2[i][j] = in.get();
+        }
+        in.get();
+    }
+    if(rotate90test(mat1, mat2, size))
+    {
+        out << 1 << endl;
+    }
+    else if(rotate180test(mat1, mat2, size))
+    {
+        out << 2 << endl;
+    }
+    else if(rotate270test(mat1, mat2, size))
+    {
+        out << 3 << endl;
+    }
+    else if(mirrortest(mat1, mat2, size))
+    {
+        out << 4 << endl;
+    }
+    else if(combotest(mat1, mat2, size))
+    {
+        out << 5 << endl;
+    }
+    else if(compare(mat1, mat2, size))
+    {
+        out << 6 << endl;
+    }
+    else
+    {
+        out << 7 << endl;
+    }
+    in.close();
+    out.close();
+}
+
+bool compare(char mat1[10][10], char mat2[10][10], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            if(mat1[i][j] != mat2[i][j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+bool rotate90test(char mat1[10][10], char mat2[10][10], int size)
+{
+    char mat3[10][10];
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat3[j][size - i - 1] = mat1[i][j];
+        }
+    }   
+    return compare(mat2, mat3, size);
+}
+bool rotate180test(char mat1[10][10], char mat2[10][10], int size)
+{
+    char mat3[10][10];
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat3[j][size - i - 1] = mat1[i][j];
+        }
+    }   
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat1[j][size - i - 1] = mat3[i][j];
+        }
+    }
+    return compare(mat2, mat1, size);
+}
+bool rotate270test(char mat1[10][10], char mat2[10][10], int size)
+{
+    char mat3[10][10];
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat3[j][size - i - 1] = mat1[i][j];
+        }
+    }   
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat1[j][size - i - 1] = mat3[i][j];
+        }
+    }
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat3[j][size - i - 1] = mat1[i][j];
+        }
+    }
+    return compare(mat2, mat3, size);
+}
+bool mirrortest(char mat1[10][10], char mat2[10][10], int size)
+{
+    char mat3[10][10];
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat3[i][size - j - 1] = mat1[i][j];
+        }
+    }
+    return compare(mat2, mat3, size);
+}
+bool combotest(char mat1[10][10], char mat2[10][10], int size)
+{
+    char mat3[10][10];
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            mat3[i][size - j - 1] = mat1[i][j];
+        }
+    }
+    if(rotate90test(mat3, mat2, size))
+    {
+        return true;
+    }
+    else if(rotate180test(mat3, mat2, size))
+    {
+        return true;
+    }
+    else if(rotate270test(mat3, mat2, size))
+    {
+        return true;
+    }
+    return false;
 }
